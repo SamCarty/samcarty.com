4b880c2eb5d61d88f20a1471cdd551d3c675b161
Sam Carty <samcarty311@gmail.com>
2019-02-03 11:07:01 +0000

    codenamemachine optimizations

--- codenamemachine/js/generate.js
+++ codenamemachine/js/generate.js
@@ -2,60 +2,40 @@ var verbs = new Array();
 var nouns = new Array();
 
 $.get('res/ListOfVerbs.txt', function (data) {
-
     verbs = data.split('\n');
     console.log(verbs);
-
 });
 
 $.get('res/ListOfNouns.txt', function (data) {
-
     nouns = data.split('\n');
     console.log(nouns);
-
 });
 
-function generate(isProject) {
-
+var generate = function (isProject) {
     clearInterval(window.intervalId);
-    
+
     console.log(verbs[0]);
     $("#word").html(verbs[(Math.floor(Math.random() * verbs.length))] + " " + nouns[(Math.floor(Math.random() * nouns.length))]);
 
     if (isProject) {
-        
         $("#type").html("<kbd>project:</kbd>");
-        
     } else {
-        
         $("#type").html("<kbd>codename:</kbd>");
-        
     }
-    
 }
 
-function loading() {
-    
-    $("#word").append(".");
-    
-}
-
-$("#btn-codename").click(function() {
+$("#btn-codename").click(function () {
 
     generate(false);
 
 });
 
-$("#btn-project").click(function() {
+$("#btn-project").click(function () {
 
     generate(true);
 
 });
 
-$(document).ready(function() {
-
-    window.intervalId = setInterval(loading, 300);
-
-    setTimeout(generate, 1000);
-    
+$(document).ready(function () {
+    generate(false);
 })
