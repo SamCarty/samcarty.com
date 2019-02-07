697d2307d9793dd7bc11bea286088e8f5e6062bb
Sam Carty <samcarty311@gmail.com>
2019-02-07 17:17:15 +0000

    use cdn to download scripts

--- codenamemachine/js/generate.js
+++ codenamemachine/js/generate.js
@@ -1,42 +1,39 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    var verbs = new Array();
-    var nouns = new Array();
+  var verbs = new Array();
+  var nouns = new Array();
 
-    var setup = function () {
-        $.get('res/ListOfVerbs.txt', function (data) {
-            verbs = data.split('\n');
-            console.log(verbs);
-        });
+  var setup = function() {
+    $.get('res/ListOfVerbs.txt', function(data) {
+      verbs = data.split('\n');
+    });
 
-        return $.get('res/ListOfNouns.txt', function (data) {
-            nouns = data.split('\n');
-            console.log(nouns);
-        });
+    return $.get('res/ListOfNouns.txt', function(data) {
+      nouns = data.split('\n');
+    });
+  }
+
+  var generate = function(isProject) {
+    clearInterval(window.intervalId);
+
+    $("#word").html(verbs[(Math.floor(Math.random() * verbs.length))] + " " + nouns[(Math.floor(Math.random() * nouns.length))]);
+
+    if (isProject) {
+      $("#type").html("<kbd>project:</kbd>");
+    } else {
+      $("#type").html("<kbd>codename:</kbd>");
     }
+  }
 
-    var generate = function (isProject) {
-        clearInterval(window.intervalId);
+  $("#btn-codename").click(function() {
+    generate(false);
+  });
 
-        console.log(verbs[0]);
-        $("#word").html(verbs[(Math.floor(Math.random() * verbs.length))] + " " + nouns[(Math.floor(Math.random() * nouns.length))]);
+  $("#btn-project").click(function() {
+    generate(true);
+  });
 
-        if (isProject) {
-            $("#type").html("<kbd>project:</kbd>");
-        } else {
-            $("#type").html("<kbd>codename:</kbd>");
-        }
-    }
-
-    $("#btn-codename").click(function () {
-        generate(false);
-    });
-
-    $("#btn-project").click(function () {
-        generate(true);
-    });
-
-    $.when(setup()).then(function() {
-        generate(false);
-    });
+  $.when(setup()).then(function() {
+    generate(false);
+  });
 })
