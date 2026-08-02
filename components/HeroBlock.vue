2aa5919f94ac4274d36d95ae36d4bcc85b3d09a5
Sam Carty <22345452+SamCarty@users.noreply.github.com>
2026-08-02 21:45:48 +0100

    Update bio text for clarity

--- components/HeroBlock.vue
+++ components/HeroBlock.vue
@@ -10,7 +10,7 @@
 
       <p class="hero-block__header__bio">
         I'm a software engineer with 6+ years of experience building mobile and web apps that deliver 
-        real-world value.
+        real value.
       </p>
 
       <p class="hero-block__header__bio">
