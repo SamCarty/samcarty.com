0f23bedafa064027ed3472fcd7d3af1642ebe472
Sam Carty <22345452+SamCarty@users.noreply.github.com>
2026-08-08 22:15:53 +0100

    Update viewport meta tag for Android chrome edge-to-edge

--- app.vue
+++ app.vue
@@ -37,7 +37,7 @@ useHead({
   ],
   meta: [
     { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
     {
       name: 'description',
       content:
